69a21e4e5dc4ce4fcf35195d2a8ac0f03ecfc5ef
Robert Williams <robert@rjava.net>
2013-07-20 18:04:54 +0000

    setup static directory

--- web.js
+++ web.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/'));
+
 app.get('/', function(request, response) {
 	var fs = require('fs');
-	var index = fs.readFileSync('index.html');
+	var index = fs.readFileSync(index.html);
 	var data = index.toString('utf-8');
 	response.send(data);
 });
